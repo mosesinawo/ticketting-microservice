8fa65a9217c47e9b98e89392d439070deaf89eff
mosesinawo <inawo.moses@gmail.com>
2025-01-24 10:45:06 +0100

    completed error handling

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
-import { RequestValidationError } from "../errors";
-import { DatabaseConnectionError } from "../errors";
+import { CustomError } from "../errors";
+
 
 export const errorHandler = (
   err: Error,
@@ -8,13 +8,13 @@ export const errorHandler = (
   res,
   next
 ) => {
-  if (err instanceof RequestValidationError) {
-    return res.status(err.statusCode).send({ errors: err.setrializeErrors() });
-  }
-
-  if (err instanceof DatabaseConnectionError) {
+  if (err instanceof CustomError) {
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
+
+//   if (err instanceof DatabaseConnectionError) {
+//     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+//   }
 
   res.status(400).send({
     errors: [{ message: "Something went wrong" }],
